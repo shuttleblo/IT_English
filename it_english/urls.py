89b896b8e1c49dd0e334c59fc2e3a174c7ec4959
Rinasham <rinasham@gmail.com>
2021-08-12 19:00:44 +1000

    add: ユーザー登録機能(デフォルト機能)

--- it_english/urls.py
+++ it_english/urls.py
@@ -6,4 +6,5 @@ app_name =  "it_english"
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('signup/', views.signup, name='signup'),
 ]
--- it_english/views.py
+++ it_english/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("仮のトップページ")
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user_instance = form.save()
+            login(request, user_instance)
+            return redirect("it_english:index")
+    else:
+        form = UserCreationForm()
+
+    context = {
+        "form": form
+    }
+    return render(request, 'signup/signup.html', context)
