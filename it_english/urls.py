af89834313bde45253f0a23557b0a780845655c2
daichi0918 <kayasugadaichi@icloud.com>
2021-08-30 20:57:27 +0900

    questionViewとurls.pyを追加

--- it_english/urls.py
+++ it_english/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from it_english.views import accountView ,word_edit_view
+from it_english.views import accountView ,word_edit_view,questionView
 
 app_name = "it_english"
 
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', accountView.account_login, name='login'),
     path('word_top/', word_edit_view.word_top, name='word_top'),
     path('word_create/', word_edit_view.word_create, name='word_create'),
-    path('word_edit/', word_edit_view.word_edit, name='word_edit')
+    path('word_edit/', word_edit_view.word_edit, name='word_edit'),
+    path('q_answer/',questionView.q_answer,name='q_answer')
 
 ]
