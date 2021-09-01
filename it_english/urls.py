fd34a55e0fbf6ccf3d716e9759a0af38f1ec3eaf
Rinasham <rinasham@gmail.com>
2021-09-02 06:57:35 +1000

    [fix] 修正

--- it_english/urls.py
+++ it_english/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path('signup/', accountView.signup, name='signup'),
     path('login/', accountView.account_login, name='login'),
     path('choice/',questionView.choiceTemplateView.as_view(),name='choice'),
-    path('result/',questionView.result,name='result'),
+    # path('result/',questionView.result,name='result'),
     path('word_top/', word_edit_view.word_top, name='word_top'),
     path('word_create/', word_edit_view.word_create, name='word_create'),
     path('word_edit/', word_edit_view.word_edit, name='word_edit'),
+    path('q_answer/',questionView.q_answer,name='q_answer'),
 ]
