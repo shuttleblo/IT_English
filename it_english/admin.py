f175886c7637e258118e1261f9b257d954466adb
Wakita <wakita.904@gmail.com>
2021-09-04 19:41:09 +0900

    user_quiz_model_add

--- it_english/admin.py
+++ it_english/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import QuizModel, UserModel
 # Register your models here.
+
+admin.site.register(UserModel)
+admin.site.register(QuizModel)
--- it_english/models.py
+++ it_english/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.db.models.fields import EmailField
 
 # Create your models here.
+
+class UserModel(models.Model):
+  name = models.CharField(max_length= 20)
+  email = models.EmailField(max_length=254)
+  password = models.CharField(max_length=50)
+  secret_question = models.CharField(max_length=100)
+
+class QuizModel(models.Model):
+  quiz = models.CharField(max_length= 20)
+  answer = models.CharField(max_length= 20)
