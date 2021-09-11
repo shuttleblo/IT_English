20d9148814f26d2cbed7110fd22128db9c9c3db3
Rinasham <rinasham@gmail.com>
2021-09-11 19:08:38 +1000

    [fix]結果をテーブルに変更

--- it_english/static/js/question/result.js
+++ it_english/static/js/question/result.js
@@ -1,21 +1,53 @@
-var correctResult =""
-var incorrectResult=""
-for(var i = 0,len = correctStorage.length; i<len; i++){
-    var k = correctStorage.key(i);
-    var v = correctStorage[k];
-    console.log(k+':'+v);
-    correctResult+=k+v;
+//正解一覧のテーブル
+var makeTableCorrect = function(){
+  var tb = document.getElementById("tbody_correct")
+  // テーブルの初期化
+  while (tb.firstChild){
+    tb.removeChild(tb.firstChild);
   }
 
-  for(var j = 0,len = incorrectStorage.length; j<len; j++){
-    var x = incorrectStorage.key(j);
-    var y = incorrectStorage[x];
-    console.log(x+':'+y);
-    incorrectResult+=x+y;
+  // テーブルの出力
+  for (var i=0; i < correctStorage.length; i++) {
+      var _key = correctStorage.key(i);
+      var tr = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      tb.appendChild(tr);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      td1.innerHTML = _key;
+      td2.innerHTML = correctStorage.getItem(_key);
   }
-  
-  document.getElementById('correct').textContent=correctResult;
+};
 
-  document.getElementById('incorrect').textContent=incorrectResult;
-
+window.addEventListener('load', function() {
+  makeTableCorrect();
   correctStorage.clear();
+})
+
+//不正解一覧のテーブル
+var makeTableIncorrect = function(){
+  var tb = document.getElementById("tbody_incorrect")
+  // テーブルの初期化
+  while (tb.firstChild){
+    tb.removeChild(tb.firstChild);
+  }
+
+  // テーブルの出力
+  for (var j=0; j < incorrectStorage.length; j++) {
+      var _key = incorrectStorage.key(j);
+      var tr = document.createElement("tr");
+      var td1 = document.createElement("td");
+      var td2 = document.createElement("td");
+      tb.appendChild(tr);
+      tr.appendChild(td1);
+      tr.appendChild(td2);
+      td1.innerHTML = _key;
+      td2.innerHTML = incorrectStorage.getItem(_key);
+  }
+};
+
+window.addEventListener('load',function(){
+  makeTableIncorrect();
+  incorrectStorage.clear();
+});
